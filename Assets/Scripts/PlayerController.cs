9a50ddb184d1e97b110ec5a30418a2e83c1edacc
LT <support@teran.tech>
2020-12-10 22:22:14 -0700

    Prototype 1 Complete
    - Finished course
    - Rough draft of game
    - Good example of physics and input controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-   public float speed = 5.0f;
+   private float speed = 35.0f;
+   private float turnSpeed = 100.0f;
+   private float horizontalInput = 0.0f;
+   private float forwardInput = 0.0f;
 
    // Start is called before the first frame update
    void Start()
@@ -15,8 +18,11 @@ public class PlayerController : MonoBehaviour
    // Update is called once per frame
    void Update()
    {
-      // Move the vehicle forward
-      transform.Translate(Vector3.forward * Time.deltaTime * speed);
+      horizontalInput = Input.GetAxis("Horizontal");
+      forwardInput = Input.GetAxis("Vertical");
 
+      // Move the vehicle forward
+      transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+      transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
    }
 }

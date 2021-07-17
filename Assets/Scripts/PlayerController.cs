6a91a52aaa1ca81b948e9b9d3ef6776b901caf34
LT <support@teran.tech>
2021-07-17 09:45:19 -0700

    Rotating vehicle tires via speed. Adding rails to prevent vehicle from falling down.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,18 @@ namespace Prototype1
    {
       float speed = 35.0f;
       float turnSpeed = 100.0f;
+      float wheelRadius = 0.5f;
       float horizontalInput = 0.0f;
       float forwardInput = 0.0f;
+
+      #region Wheels
+      Transform[] wheels = new Transform[4];
+      #endregion
 
       // Start is called before the first frame update
       void Start()
       {
-
+         GetVehicleWheels();
       }
 
       // Update is called once per frame
@@ -30,6 +35,30 @@ namespace Prototype1
          // Move the vehicle forward
          transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
          transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+         float distanceTraveled = speed * Time.deltaTime;
+         float rotationInRadians = distanceTraveled / wheelRadius;
+         float rotationInDegrees = rotationInRadians * Mathf.Rad2Deg;
+         SpinVehicleWheels(rotationInDegrees);
+      }
+
+      void GetVehicleWheels()
+      {
+         // There should only ever be 4 wheels
+         Debug.Assert(transform.childCount == 4);
+
+         for (int i = 0; i < transform.childCount; i++)
+         {
+            wheels[i] = transform.GetChild(i);
+         }
+      }
+
+      void SpinVehicleWheels(float rotationDegree)
+      {
+         wheels[0].Rotate(rotationDegree, 0, 0);
+         wheels[1].Rotate(rotationDegree, 0, 0);
+         wheels[2].Rotate(rotationDegree, 0, 0);
+         wheels[3].Rotate(rotationDegree, 0, 0);
       }
    }
 }
